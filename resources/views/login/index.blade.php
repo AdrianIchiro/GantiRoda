726beb43cacc699961f00de8923e7e083155f08d
AdrianIchiro <adrianfahren10@gmail.com>
2025-01-18 11:03:22 +0700

    membuat fitur login dan logout

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -32,7 +32,7 @@
 </head>
 
 <body>
-    <div class="container-xxl position-relative bg-white d-flex p-0">
+ <div class="container-xxl position-relative bg-white d-flex p-0">
         <!-- Spinner Start -->
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
@@ -42,7 +42,7 @@
         <!-- Spinner End -->
 
 
-        <!-- Sign Up Start -->
+        <!-- Sign In Start -->
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
@@ -51,52 +51,33 @@
                             <a href="index.html" class="">
                                 <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>PindahRoda</h3>
                             </a>
-                            <h3>Sign Up</h3>
+                            <h3>Sign In</h3>
                         </div>
-                        @if (session()->has('success'))
-                            <div class="alert alert-success" role="alert">
-                                {{session('success')}}
-                            </div>
-                        @endif
-                        <form action="{{route('register.store')}}" method="POST">
+                        <form action="{{route('login.proses')}}" method="POST">
                             @csrf
-                            @error('name')
-                                <span class="text-danger">{{$message}}</span>
-                            @enderror
-                            <div class="form-floating mb-3">
-                                <input type="text" name="name" class="form-control" id="floatingText" placeholder="jhondoe" value="{{@old('name')}}">
-                                <label for="floatingText">Name</label>
-                            </div>
                             @error('email')
                                 <span class="text-danger">{{$message}}</span>
                             @enderror
                             <div class="form-floating mb-3">
-                                <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com" value="{{@old('email')}}">
+                                <input type="email" name="email" class="form-control" id="floatingInput" value="{{@old('email')}}" placeholder="name@example.com">
                                 <label for="floatingInput">Email address</label>
                             </div>
                             @error('password')
                                 <span class="text-danger">{{$message}}</span>
                             @enderror
                             <div class="form-floating mb-4">
-                                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" value="{{@old('password')}}">
+                                <input type="password" name="password" class="form-control" id="floatingPassword" value="{{@old('password')}}" placeholder="Password">
                                 <label for="floatingPassword">Password</label>
                             </div>
-                            <div class="d-flex align-items-center justify-content-between mb-4">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                </div>
-                                <a href="">Forgot Password</a>
-                            </div>
-                            <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign Up</button>
-                            <p class="text-center mb-0">Already have an Account? <a href="{{route('login')}}">Sign In</a></p>
+                            <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
                         </form>
 
+                        <p class="text-center mb-0">Don't have an Account? <a href="{{route('register')}}">Sign Up</a></p>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Sign Up End -->
+        <!-- Sign In End -->
     </div>
 
     <!-- JavaScript Libraries -->
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,7 +11,12 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'proses'])->name('login.proses');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
